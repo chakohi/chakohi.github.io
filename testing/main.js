d91727345f8dc1e260153ed0c3d987c89e8244b1
Adham Chakohi <swagbook@swagbook.local>
2020-09-17 16:10:13 +0400

    added testing website

---
+++ testing/main.js
@@ -0,0 +1,22 @@
+var top = document.getElementById("top");
+
+document.onscroll = function() {
+
+    scrollTop = window.pageYOffset;
+    top.innerHTML = scrollTop;
+
+    allDivs = document.getElementsByClassName('section-divider');
+    for (i = 0; i < allDivs.length; i++) {
+        curDiv = allDivs[i];
+        heightBefore = 0;
+        if (i > 0) {
+            heightBefore = allDivs[i - 1].offsetHeight / 3;
+        }
+
+        if (scrollTop > curDiv.offsetTop - heightBefore) {
+            color = curDiv.getAttribute("data-color");
+            document.body.style.backgroundColor = color;
+        }
+
+    }
+};
